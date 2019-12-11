8f7ec1f8221c0a70043531801fe112ac1a359c98
Andre Shindi Fucuhara <asFucu@Shindips-MacBook-Pro.local>
2019-12-11 13:40:07 -0300

    Creating Config files and removing sensitive info

---
+++ config/Dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mail: {
+    user: 'asfucuhara@live.com',
+    pass: 'DipperP!nes',
+    receiver: 'asfucuhara@kindle.com'
+  },
+  minifiedFolder:
+    '/Users/asFucu/Documents/Projects/Node/EmailSender/Files/Minified',
+  toProcessFolder:
+    '/Users/asFucu/Documents/Projects/Node/EmailSender/Files/ToProcess'
+};
--- file-Watcher.js
+++ file-Watcher.js
@@ -2,14 +2,17 @@ const chokidar = require('chokidar');
 const fs = require('fs');
 const shelljs = require('shelljs');
 
+const { toProcessFolder, minifiedFolder } = require('./config');
 const mail = require('./mail');
 
-const folder = '/Users/asFucu/Documents/Projects/Node/EmailSender/Files';
-
 chokidar
-  .watch('./ToProcess/', { awaitWriteFinish: true, ignored: /(^|[\/\\])\../ })
+  .watch(toProcessFolder, {
+    awaitWriteFinish: true,
+    ignored: /(^|[\/\\])\../
+  })
   .on('add', (path, stats) => {
-    if (path.indexOf(' ') !== -1 || path.indexOf('\'') !== -1 ) {
+    if (path.indexOf(' ') !== -1 || path.indexOf("'") !== -1) {
+      //change to process paths with white spaces
       //trim string and wait for another call of add on chokidar to execute
       fs.rename(`./${path}`, `./${path.replace(/ |\'/g, '')}`, err => {
         if (err) throw err;
@@ -19,26 +22,29 @@ chokidar
     }
 
     //if trimmed execute the flow
-    const file = path.substring(10);
-    console.log(`Chiokidar: new file at ${path}`);
+    const file = path.substring(toProcessFolder.length + 1); //todo get file name{get everything afee last /}
+    console.log(`Chiokidar: new file ${file}`);
+
 
     //see if file exist in minified folder
-    fs.access(`./Minified/${file}`, fs.constants.F_OK, err => {
+    fs.access(`${minifiedFolder}/${file}`, fs.constants.F_OK, err => {
       if (err) {
-        const script = `gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4  -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=${folder}/Minified/${file} ${folder}/ToProcess/${file}`;
+        //todo Bundle Chapters in Volume too
+        const script = `gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4  -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=${minifiedFolder}/${file} ${toProcessFolder}/${file}`;
         console.log('exec:', script);
-        console.log(`minifying: ${path}`);
+        console.log(`minifying: ${file}`);
         shelljs.exec(script, code => {
           if (!code) {
-            console.log(`Minified succeed: ${path}`);
+            console.log(`Minified succeed: ${file}`);
             console.log(`Sending ${file} to Amazon Kindle`);
-            
+
             //send Mail to amazon storage
-            mail(`${folder}/Minified/${file}`)
+            mail(`${minifiedFolder}/${file}`);
           }
         });
+        đ;
       } else {
-        console.log(`Already minified: ${path}`);
+        console.log(`Already minified: ${file}`);
       }
     });
     //console.log(path, stats);
