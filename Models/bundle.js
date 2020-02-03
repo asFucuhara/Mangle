a14a620a61e1d67a9a8338d250bf4f0c275a5d0f
Andre Shindi Fucuhara <asFucu@Shindips-MacBook-Pro.local>
2020-02-03 15:36:30 -0300

    creation of bundle model

---
+++ Models/bundle.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bundleSchema = new Schema({
+  name: String,
+  bundled: Array,
+  path: String,
+  dateBundled: Date
+});
+
+mongoose.model('Bundle', bundleSchema);
